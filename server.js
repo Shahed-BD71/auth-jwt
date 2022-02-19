06f47fecaf19848e864a477fcde2b045cccb6f02
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-02-19 15:03:31 +0600

    done

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ app.use("/auth", require("./routes/user"));
 
 // server build
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/dist'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Hello from the server side!!", PORT));

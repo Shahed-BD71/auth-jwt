a343af4b7ad5aef19697ea84033c4d41ae7b96fa
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-02-19 12:41:30 +0600

    update to client

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.use(cors());
 app.use("/auth", require("./routes/user"));
 
 // server build
-if (process.env.NODE_ENV === "production ") {
-  app.use(express.static("frontend/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 

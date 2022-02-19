47dc1a9a71a736bba9c891d105b5ac90d62a5d87
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-02-19 15:04:15 +0600

    done

--- server.js
+++ server.js
@@ -30,5 +30,5 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Hello from the server side!!", PORT));

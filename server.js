f8fabd81f7cb7cea61b80b8d1777813d9b7fa5ad
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-02-19 11:01:28 +0600

    make it better

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-var cors = require('cors')
+var cors = require("cors");
 const app = express();
+const path = require('path')
 
 // connect database.
-mongoose.connect(process.env.DB_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-},(err) => {
-  if(err) throw err; 
-  console.log('Connected to MongoDB!!!')
-}) 
+mongoose.connect(
+  process.env.DB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB!!!");
+  }
+);
 
 app.use(express.json());
-app.use(cors())
-app.use('/auth', require('./routes/user'));
+app.use(cors());
+app.use("/auth", require("./routes/user"));
+
+// server build
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log("Hello from the server side!!", PORT));

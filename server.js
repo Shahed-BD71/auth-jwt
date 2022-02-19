ce60f6a431ec10bb06bad550b9d3c6597a06989a
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-02-19 11:14:24 +0600

    done

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/auth", require("./routes/user"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 

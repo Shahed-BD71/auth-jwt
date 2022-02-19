72237f657ae53e3eea594f63876638cad0084dde
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-02-19 16:26:58 +0600

    fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
-import axios from "axios";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './components/Auth/Login';
 import Register from "./components/Auth/Register";
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+// axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 function App() {
   return (
     <div>
